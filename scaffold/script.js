0c56fbac1c867518d2a8ea01a5f1f959648ad255
Bruno Dias <bruno.r.dias@gmail.com>
2016-10-12 20:23:40 -0300

    Support for user scripts and blotter changes

---
+++ scaffold/script.js
@@ -0,0 +1,8 @@
+/*
+  This script file runs after Blotter's main bundle and can contain code
+  to support custom UI and logic.
+
+  The main story object from inkjs is present in window.story.
+*/
+
+window.blotterStart(); // Start up Blotter
